d42bd48d71c64a393670799076be5d5ec4b1e6d2
Eloim <eloimdavid29@gmail.com>
2023-03-26 13:23:11 -0500

    agregacion de carpetas

--- ProyectoUI/FormLogin.Designer.cs
+++ ProyectoUI/FormLogin.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProyectoUI
 {
-    partial class Form1
+    partial class FormLogin
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(859, 450);
+            this.Name = "FormLogin";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
